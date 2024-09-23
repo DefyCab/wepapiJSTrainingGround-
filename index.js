06ac7d016310db5836ec84963fded839a60f6534
defycab <love.bellinder@gmail.com>
2024-09-23 11:23:59 +0200

    fiddling with the requset and error handling

--- index.js
+++ index.js
@@ -2,11 +2,31 @@ import http from "http"
 import fs from "fs"
 
 const server = http.createServer((req, res) => {
-  const content = fs.readFileSync("./static/index.html", "utf-8")
+  const fileNameOfUrl = (url) => {
+    let fileName = ""
+    if (req.url.split("/")[1] === "") {
+      fileName = "index.html"
+    } else {
+      fileName = req.url.split("/")[1]
+    }
+    return fileName
+  }
+
+  const fileName = fileNameOfUrl(req.url)
+  if (fileName === "favicon.ico") {
+    res.statusCode = 404
+    res.end("")
+    return
+  }
+
+  const content = fs.readFileSync(`./static/${fileName}`, "utf-8")
 
   res.statusCode = 200
   res.setHeader("Content-Type", "text/html")
   res.end(content)
+
+  console.log(`The URL for the request was '${req.url}'`)
+  console.log(`The method for the request was '${req.method}`)
 })
 
 const hostname = "localhost"
