92d01577ef5675358fb4765ee5cb1c79a28c3fcb
defycab <love.bellinder@gmail.com>
2024-09-23 10:18:44 +0200

    changing to type module and creating server

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Started again and once more")
+import http from "http"
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader("Content-Type", "text/plain")
+  res.end("Hello fellow developer!")
+})
+
+const hostname = "localhost"
+const port = 3000
+
+server.listen(port, hostname, () => {
+  console.log(`Server is runnning at http://${hostname}:${port}/`)
+})
