478285e3922898410dbd2dbf8e4c8982f767e3d2
defycab <love.bellinder@gmail.com>
2024-09-23 10:33:46 +0200

    returning index.html

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import http from "http"
+import fs from "fs"
 
 const server = http.createServer((req, res) => {
+  const content = fs.readFileSync("./static/index.html", "utf-8")
+
   res.statusCode = 200
   res.setHeader("Content-Type", "text/html")
-  res.end("<h2>Hello fellow developer!</h2>")
+  res.end(content)
 })
 
 const hostname = "localhost"
