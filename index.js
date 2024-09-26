81de6d6af5b82d7d62fdff85d3547c9adf795166
defycab <love.bellinder@gmail.com>
2024-09-26 12:13:10 +0200

    adding app.use static to access static folder pages

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-import express from 'express';
-const app = express();
+import express from "express"
+const app = express()
+app.use(express.static("static"))
 
-app.get('/', (req, res) => {
-  res.send('Hello fellow developer!');
-});
+app.get("/", (req, res) => {
+  res.send("Hello fellow developer!")
+})
 
 const port = 3000
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
-});
+  console.log(`Server running at http://localhost:${port}/`)
+})
