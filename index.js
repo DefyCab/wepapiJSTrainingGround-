1b29561636f0157e6d2f11c9d0f72c0c7bdaf12e
defycab <love.bellinder@gmail.com>
2024-09-26 13:28:36 +0200

    writing code to post developer do db

--- index.js
+++ index.js
@@ -17,9 +17,20 @@ const db = [
 app.use(express.json())
 
 app.post("/api/developers/", (req, res) => {
-  console.log(req.body)
-  res.status(201).end()
+  const newDeveloper = {
+    id: 3,
+    name: req.body.name,
+    email: req.body.email,
+  }
+
+  db.push(newDeveloper)
+
+  res
+    .status(201)
+    .setHeader("location", `/api/developers/${newDeveloper.id}`)
+    .json(newDeveloper)
 })
+
 app.get("/api/developers/:id", (req, res) => {
   const developer = db.find((developer) => developer.id == req.params.id)
   if (!developer) {
@@ -27,6 +38,10 @@ app.get("/api/developers/:id", (req, res) => {
     return
   }
   res.json(developer)
+})
+
+app.get("/api/developers", (req, res) => {
+  res.json(db)
 })
 
 const port = 3000
